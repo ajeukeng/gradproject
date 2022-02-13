57872091683e508c9d1b8033d76da4c1b0519bc4
jeukengl <lizjeukeng@Lizs-MacBook-Pro.local>
2022-02-12 20:50:54 -0700

    able to query data

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,7 @@
 import click
-from CovidVIsualizationTool import CovidVisualizationTool as CVT
-from imports.importData import ImportData as ID
+from covidVisualizationTool.imports.importData import ImportData as ID
 import common_utilities as cu
+from covidVisualizationTool.queries.queryData import QueryData as QD
 
 
 @click.group()
@@ -16,7 +16,9 @@ def cvt():
 def import_data(csv_location, db_name: str):
     # Import Covid Dataset
     if cu.check_path(csv_location):
-        id_covid = ID(csv_location, db_name)
-        id_covid.load_tables()
+        # id_covid = ID(csv_location, db_name)
+        # id_covid.load_tables()
+        query_stuff = QD(db_location=db_name)
+        query_stuff.get_all_data()
     else:
         print("Unable to find path")
---
+++ src/covidVisualizationTool/queries/queryData.py
@@ -0,0 +1,47 @@
+from covidVisualizationTool.imports.base import dbBase
+from covidVisualizationTool.imports.models import *
+
+
+class QueryData(dbBase):
+    def __init__(self, db_location):
+        super().__init__(db_location)
+        self.db_location = db_location
+
+    def get_all_data(self):
+        """Query to retrieve all distinct artifacts"""
+        all_data = self.session.query(Location.location).distinct()
+        print(all_data)
+        for r in all_data:
+            print(r)
+
+    def get_death_rate_partially_vaccinated(self):
+        """Query to retrieve the death rate vs number of partially vaccinated individuals over time"""
+        pass
+
+    def get_death_rate_fully_vaccinated(self):
+        """Query to retrieve the death rate vs number of fully vaccinated individuals over time"""
+        pass
+
+    def get_positive_rate_by_population_density(self):
+        """Query to retrieve positivity rate based on population density over time"""
+        pass
+
+    def get_median_age_death_rate(self):
+        """Query to retrieve number median age vs number of deaths over time"""
+        pass
+
+    def get_positive_rate_for_total_test(self):
+        """Query to retrieve the number of positive tests vs total tests"""
+        pass
+
+    def get_icu_patients_vaccinations(self):
+        """Query to retrieve the number of icu patients vs total vaccinations over time"""
+        pass
+
+    def get_boosted_positive_rate(self):
+        """Query to retrieve the number of boosted vs positivity rate over time"""
+        pass
+
+    def get_population_vaccinated(self):
+        """Query to retrieve the correlation between population and likelihood to get vaccinated"""
+        pass
